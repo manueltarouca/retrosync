5d31eea57be993c3eef9d511fd9b332ebb06763a
mt <manuel+git@matm.xyz>
2023-01-10 23:56:44 +0000

    Added confirm dialog

--- app/src/utils/files.ts
+++ app/src/utils/files.ts
@@ -3,14 +3,15 @@ import path from 'path';
 
 export function search(dir: string, extension: string) {
   const list = fs.readdirSync(dir);
+  const paths = new Array<string>();
   list.forEach((file) => {
     const filePath = path.resolve(dir, file);
     const statResult = fs.statSync(filePath);
     if (statResult && statResult.isDirectory()) {
-      search(filePath, extension);
+      paths.push(...search(filePath, extension));
     } else if (path.extname(filePath) === extension) {
-      console.log(filePath)
-      return filePath;
+      paths.push(filePath);
     }
   });
+  return paths;
 }
