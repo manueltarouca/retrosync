5a8fdc827241aab75d237301fb332bfa9427e06e
mt <manuel+git@matm.xyz>
2023-01-15 23:08:03 +0000

    Added backend endpoint for all user files

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,23 +15,47 @@ const app = express();
 app.use(cors());
 
 app.post('/upload', upload.array('file'), async (req, res) => {
-  if (req.files && req.body.filedata) {
-    const fileData: FileData[] = req.body.filedata.map((item: string) => JSON.parse(item));
-    const ops = (req.files as Express.Multer.File[]).map(file => {
-      const current = fileData.find(item => item.name === file.originalname);
-      return prisma.saves.create({
-        data: {
-          name: file.filename,
-          originalName: file.originalname,
-          path: file.path,
-          originalPath: current?.path,
-          userId: 1
-        }
+  try {
+    if (req.files && req.body.filedata) {
+      const fileData: FileData[] = req.body.filedata.map((item: string) => JSON.parse(item));
+      const ops = (req.files as Express.Multer.File[]).map(file => {
+        const current = fileData.find(item => item.name === file.originalname);
+        return prisma.saves.create({
+          data: {
+            name: file.filename,
+            originalName: file.originalname,
+            path: file.path,
+            originalPath: current?.path,
+            userId: 1
+          }
+        });
       });
-    });
-    console.log(`to insert ${ops.length} items`)
-    await Promise.all(ops);
+      console.log(`to insert ${ops.length} items`)
+      await Promise.all(ops);
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' })
   }
+
+});
+
+app.get('/files', async (req, res) => {
+  try {
+    const userId = 1;
+    const saves = await prisma.saves.findMany({
+      where: {
+        userId
+      },
+      select: {
+        originalName: true,
+        originalPath: true
+      }
+    });
+    res.json({ items: saves });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' })
+  }
+
 });
 
 app.listen(PORT, () => {
